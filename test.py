988a5a6f15f45c170c35d77659fc63d064e0f628
Gianguido Sorà <gianguidorama@gmail.com>
2014-06-14 01:52:47 +0200

    prints access token and refresh token

---
+++ test.py
@@ -0,0 +1,10 @@
+import pyimgur
+
+testImage = open('./test.png', 'r')
+secret = "YOUR SECRET"
+clientID = "YOUR CLIENT ID"
+
+print("Go to {}, paste here your PIN and press enter.".format(pyimgur.craftAuthURL(clientID)))
+PIN = input("PIN >> ")
+acc, rec = pyimgur.imgurAuth(clientID, secret, PIN)
+print("Access token: {}\nRefresh token: {}".format(acc, rec))
